3a83b93e3311fa636c211f3c8ac99daa42c75de1
Anthony Torres <anthony.torres.p@gmail.com>
2023-02-22 14:07:07 -0300

    Storage local

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+window.addEventListener('load', () => {
+    if (localStorage.getItem('carrito')) {
+      carritoDeCompras = JSON.parse(localStorage.getItem('carrito'));
+      carritoDeCompras.forEach(producto => agregarFilaCarrito(producto));
+    }
+  });
+
 // Variables 
 
 let carritoDeCompras = []
@@ -30,15 +37,16 @@ let listaDeProductos = [];
 function agregarAlCarrito(id) {
     let productoEncontrado = listaDeProductos.find(producto => producto.id === id);
     if (productoEncontrado) {
-        carritoDeCompras.push(productoEncontrado);
-        agregarFilaCarrito(productoEncontrado);
-
-        console.log(`El producto ${productoEncontrado.nombre} ha sido agregado al carrito.`);
-
+      carritoDeCompras.push(productoEncontrado);
+      agregarFilaCarrito(productoEncontrado);
+  
+      console.log(`El producto ${productoEncontrado.nombre} ha sido agregado al carrito.`);
+  
+      localStorage.setItem('carrito', JSON.stringify(carritoDeCompras));
     } else {
-        console.log(`El producto con id ${id} no existe.`);
+      console.log(`El producto con id ${id} no existe.`);
     }
-}
+  }
 
 // Funcion que crea las Filas en la Pagina HTML
 
@@ -90,23 +98,24 @@ function eliminarProducto(id) {
       eliminarFilaCarrito(index);
       carritoDeCompras.splice(index, 1);
       console.log(`El producto con ID ${id} ha sido eliminado del carrito.`);
+  
+      localStorage.setItem('carrito', JSON.stringify(carritoDeCompras));
     } else {
       console.log(`El producto con ID ${id} no se encuentra en el carrito.`);
     }
-}
-
+  }
 // Los Objetos  
 
 const comida1 = new ComidaMascota(1 ,"Comida para gato Indoor","Equilibrio","3KG",7000);
-const comida2 = new ComidaMascota(2,"Comida para gato Indoor","equilibrio","6KG",14000);
-const comida3 = new ComidaMascota(3,"Comida para gatitos","naturalis","3KG",5000);
+const comida2 = new ComidaMascota(2,"Comida para gato Indoor","Equilibrio","6KG",14000);
+const comida3 = new ComidaMascota(3,"Comida para gatitos","Naturalis","3KG",5000);
 const comida4 = new ComidaMascota(4,"comida para perro","Alaska","15KG",20000);
-const comida5 = new ComidaMascota(5,"Comida para gato control de pelo","hills","3KG",8000);
-const comida6 = new ComidaMascota(6,"comida para gato control de pelo","hilss","6KG",15000);
-const comida7 = new ComidaMascota(7,"comida para gato control de pelo","hills","9KG",21000);
-const comida8 = new ComidaMascota(8,"comida para gato Indoor","hills","3KG",5000);
-const comida9 = new ComidaMascota(9,"comida para perro articular","mv","10KG",22000);
-const comida10 = new ComidaMascota(10,"comida para perro cardio","mv","10KG",23000);
+const comida5 = new ComidaMascota(5,"Comida para gato control de pelo","Hills","3KG",8000);
+const comida6 = new ComidaMascota(6,"comida para gato control de pelo","Hills","6KG",15000);
+const comida7 = new ComidaMascota(7,"comida para gato control de pelo","Hills","9KG",21000);
+const comida8 = new ComidaMascota(8,"comida para gato Indoor","Hills","3KG",5000);
+const comida9 = new ComidaMascota(9,"comida para perro articular","Mv","10KG",22000);
+const comida10 = new ComidaMascota(10,"comida para perro cardio","Mv","10KG",23000);
 
 // Push de Objetos a la lista de productos
 
